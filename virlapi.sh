56c3c93d4e155d2440d253d1aa0808260e6c02a1
candle <shorty@getent.ch>
2020-05-13 16:09:24 +0200

    Update virlapi.sh

--- virlapi.sh
+++ virlapi.sh
@@ -5,9 +5,10 @@ set -e
 LAB=$1
 USER="username"
 IP="x.x.x.x"
+JWT="xxxxxxxxxxxxxxxxxxxxxxxxxx"
 URL="https://$IP/api/v0/labs"
 OPT="accept: application/json"
-BEAR="Authorization: Bearer xxx"
+BEAR="Authorization: Bearer $JWT"
 
 NODES=$(curl -s -k -X GET "$URL/$LAB/nodes" -H "$OPT1" -H "$BEAR" | jq -r '.[]' )
 
