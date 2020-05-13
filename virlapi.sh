b91cf5f00afa351ea3e6977202f093c7cc1fc35f
candle <shorty@getent.ch>
2020-05-13 16:08:45 +0200

    div. changes

--- virlapi.sh
+++ virlapi.sh
@@ -3,7 +3,9 @@
 set -e
 
 LAB=$1
-URL="https://172.16.0.21/api/v0/labs"
+USER="username"
+IP="x.x.x.x"
+URL="https://$IP/api/v0/labs"
 OPT="accept: application/json"
 BEAR="Authorization: Bearer xxx"
 
@@ -11,6 +13,6 @@ NODES=$(curl -s -k -X GET "$URL/$LAB/nodes" -H "$OPT1" -H "$BEAR" | jq -r '.[]' 
 
 for N in ${NODES}
   do
-    mate-terminal --tab -t $N -e "ssh -t thalo@172.16.0.21 open /${1}/${N}/0"
+    mate-terminal --tab -t $N -e "ssh -t $USER@$IP open /${1}/${N}/0"
 
   done
