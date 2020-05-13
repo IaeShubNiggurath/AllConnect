156bb2d54f7c20afff564eaa04571a6f1f1b481f
candle <shorty@getent.ch>
2020-05-13 15:20:19 +0200

    Update virlapi.sh

--- virlapi.sh
+++ virlapi.sh
@@ -5,7 +5,7 @@ set -e
 LAB=$1
 URL="https://172.16.0.21/api/v0/labs"
 OPT="accept: application/json"
-BEAR="Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb20uY2lzY28udmlybCIsImlhdCI6MTU4OTM0NTg0OCwiZXhwIjoxNTg5NDMyMjQ4LCJzdWIiOiIxMDEifQ.tRO77u3LVnKwsvaUKyCapm3691066N5wCjrCHMe3sIE"
+BEAR="Authorization: Bearer xxx"
 
 NODES=$(curl -s -k -X GET "$URL/$LAB/nodes" -H "$OPT1" -H "$BEAR" | jq -r '.[]' )
 
